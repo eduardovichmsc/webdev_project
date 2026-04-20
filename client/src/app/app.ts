32902434bc2dc95edddb98f38e86bd8c23ebec7b
Yenlik <enilik.erkin24@gmail.com>
2026-04-20 22:52:15 +0500

    added authorization and profile; edited: shop

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/layout/header/header';
 import { CartDrawer } from './shared/components/cart-drawer/cart-drawer';
+import { Footer } from './shared/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, CartDrawer],
+  imports: [RouterOutlet, Header, CartDrawer, Footer],
   templateUrl: './app.html',
 })
 export class App {
--- client/src/app/shared/layout/footer/footer.ts
+++ client/src/app/shared/layout/footer/footer.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ProductService } from '../../services/product/product';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './footer.html',
-  styleUrl: './footer.css',
 })
-export class Footer {
+export class Footer implements OnInit {
+  constructor(private productService: ProductService) {}
 
+  categories = signal<Category[]>([]);
+
+  ngOnInit(): void {
+    this.productService.getProductCategories().subscribe({
+      next: (cats) => this.categories.set(cats),
+      error: () => {},
+    });
+  }
+
+  helpLinks = [
+    { name: 'Shipping' },
+    { name: 'Returns & Exchanges' },
+    { name: 'Size Guide' },
+    { name: 'FAQ' },
+    { name: 'Contact us' },
+  ];
+
+  legalLinks = ['Privacy Notice', 'Cookie Policy', 'Privacy Policy'];
 }
