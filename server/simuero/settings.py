1a34bd4aae40160d9563ac7d99d85b88685b6b71
eduardovichmsc <eduardovdarzhan@gmail.com>
2026-04-21 23:44:32 +0500

    Update CORS for test deployment

--- server/simuero/settings.py
+++ server/simuero/settings.py
@@ -78,12 +78,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5000',
-    'http://127.0.0.1:5000',
-    'http://localhost:4200',
-    'http://127.0.0.1:4200',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
